121ea58ae854fcbf16f814e727282b58fac235eb
mmpacker <mmpacker@gmail.com>
2021-03-26 23:52:49 -0400

    Set up main views, routes, controllers.

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,8 +1,49 @@
 const Course = require('../models/course');
+const User = require('../models/user');
 
 module.exports = {
     index,
+    myList,
+    new: newCourse,
+    search,
+    find: findCourse,
 }
+
+function findCourse(req, res) {
+    Course.find({ name: req.body.name })
+        .then(courses => {
+            res.render('courses/search', {
+                title: 'Search Courses',
+                user: req.user,
+                courses
+            })
+        })
+}
+
+function search(req, res) {
+    res.render('courses/search', {
+        title: 'Search Courses',
+        user: req.user
+    })
+}
+
+function newCourse(req, res) {
+    res.render('courses/new', {
+        user: req.user,
+        title: 'Add a New Course'
+    })
+}
+
+function myList(req, res) {
+    Course.find({ playedBy: req.user._id })
+    .then((courses) => {
+      res.render('courses/mylist', {
+        title: "My Golf Courses",
+        user: req.user,
+        courses
+      })
+    })
+  }
 
 function index(req, res) {
     Course.find({})
@@ -12,7 +53,6 @@ function index(req, res) {
             title: 'All Golf Courses',
             courses,
             user: req.user,
-            users
         })
     })
 }
--- routes/courses.js
+++ routes/courses.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 const coursesCtrl = require('../controllers/courses')
 
 router.get('/', isLoggedIn, coursesCtrl.index)
+router.get('/mylist', isLoggedIn, coursesCtrl.myList)
+router.get('/new', isLoggedIn, coursesCtrl.new)
+router.get('/search', isLoggedIn, coursesCtrl.search)
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
